885a8f44c5192dc64ce7156e7cbe688fea215786
sathyamus <sathyamus.sify@gmail.com>
2020-02-02 02:09:37 +0530

    (feature) : Added Logger

--- server-user-management/src/main/java/com/sha/serverusermanagement/repository/UserRepository.java
+++ server-user-management/src/main/java/com/sha/serverusermanagement/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.sha.serverusermanagement.repository;
 
 import com.sha.serverusermanagement.model.User;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;

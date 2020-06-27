69c509d2146dd559d11deb78f1199b9849785ea4
sathyamus <sathyamus.sify@gmail.com>
2020-06-28 00:52:11 +0530

    Results view partially integrated with Back-end Service

--- mobile-user-management/client/src/app/exam-result/exam-result.page.ts
+++ mobile-user-management/client/src/app/exam-result/exam-result.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
+import { ResultsService } from '../services/school/results.service';
+import { Result } from '../model/result';
 
 @Component({
   selector: 'app-exam-result',
@@ -6,53 +10,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exam-result.page.scss'],
 })
 export class ExamResultPage implements OnInit {
-  marks = [
-    {
-      subject: 'Math',
-      mark: 80,
-      grade: 'A',
-      commnet: 'Very good'
-    },
-    {
-      subject: 'Yellow Belt',
-      mark: 93,
-      grade: 'A+',
-      commnet: 'Excellent'
-    },
-    {
-      subject: 'Science',
-      mark: 85,
-      grade: 'A',
-      commnet: 'Very good'
-    },
-    {
-      subject: 'English',
-      mark: 87,
-      grade: 'A',
-      commnet: 'Very good'
-    },
-    {
-      subject: 'Advnace Technology',
-      mark: 75,
-      grade: 'A-',
-      commnet: 'Greate'
-    },
-    {
-      subject: 'Sanskrit',
-      mark: 80,
-      grade: 'A',
-      commnet: 'Very good'
-    },
-    {
-      subject: 'Computer',
-      mark: 90,
-      grade: 'A',
-      commnet: 'Excellent'
-    }
-  ]
-  constructor() { }
 
-  ngOnInit() {
-  }
+  results: Array<Result>;
+  constructor(
+    private resultsService: ResultsService,
+    private router: Router,
+    private menuController: MenuController) { }
+
+  /*
+    marks = [
+      {
+        subject: 'Math',
+        mark: 80,
+        grade: 'A',
+        commnet: 'Very good'
+      },
+      {
+        subject: 'Yellow Belt',
+        mark: 93,
+        grade: 'A+',
+        commnet: 'Excellent'
+      },
+      {
+        subject: 'Science',
+        mark: 85,
+        grade: 'A',
+        commnet: 'Very good'
+      },
+      {
+        subject: 'English',
+        mark: 87,
+        grade: 'A',
+        commnet: 'Very good'
+      },
+      {
+        subject: 'Advnace Technology',
+        mark: 75,
+        grade: 'A-',
+        commnet: 'Greate'
+      },
+      {
+        subject: 'Sanskrit',
+        mark: 80,
+        grade: 'A',
+        commnet: 'Very good'
+      },
+      {
+        subject: 'Computer',
+        mark: 90,
+        grade: 'A',
+        commnet: 'Excellent'
+      }
+    ]
+  
+    */
+
+    ngOnInit() {
+      this.menuController.enable(true);
+      this.findResultsByStudentIdAndClass(100, 2020);
+    }
+  
+    findResultsByStudentIdAndClass(studentId : number, classGrade : number) {
+      this.resultsService.findResultsByStudentIdAndClass(studentId, classGrade).subscribe(data => {
+        this.results = data;
+      });
+    }
+  
 
 }
---
+++ mobile-user-management/client/src/app/services/school/results.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Result } from '../../model/result';
+import { User } from '../../model/user';
+import { properties } from 'src/init';
+
+let API_URL = properties.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ResultsService {
+  currentUser: User;
+  headers: HttpHeaders;
+  constructor(private http: HttpClient) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.headers = new HttpHeaders({
+      authorization: 'Bearer ' + this.currentUser.token,
+      "Content-Type": "application/json; charset=UTF-8"
+    });
+  }
+
+  logOut(): Observable<any> {
+    return this.http.post(API_URL + "/user/logout", {})
+      .pipe(map(response => {
+        localStorage.removeItem('currentUser');
+      }));
+  }
+
+  findAllResults(): Observable<any> {
+    return this.http.get(API_URL + "/api/v1/results", { headers: this.headers });
+  }
+
+  findResultsByStudentIdAndClass(studentId : number, classGrade : number): Observable<any> {
+    return this.http.get(API_URL + "/api/v1/results/" + studentId + "/" + classGrade, { headers: this.headers });
+  }
+}

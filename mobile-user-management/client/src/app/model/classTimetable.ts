161c45000fed70cd4043a5026b19a941137c0caf
sathyamus <sathyamus.sify@gmail.com>
2020-07-07 22:13:45 +0530

    (feature): Class Timetable view integrated with Back-end Service

---
+++ mobile-user-management/client/src/app/model/classTimetable.ts
@@ -0,0 +1,13 @@
+
+export class ClassTimetable {
+  code: string = "";
+  classGrade: number;
+  section: string = "";
+  year: number; 
+  semester: string = "";
+  subject: string = ""; 
+  classRoom: string = ""; 
+  date: string = ""; 
+  time: string = "";
+  color: string = ""; 
+}

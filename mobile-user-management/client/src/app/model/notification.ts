45869d0aa0c6dec0e8deb2a9fff55e7221517933
sathyamus <sathyamus.sify@gmail.com>
2020-07-25 23:53:19 +0530

    (feature): Added notification.service.ts, model

---
+++ mobile-user-management/client/src/app/model/notification.ts
@@ -0,0 +1,8 @@
+
+export class Notification {
+  notificationsId: number;
+  notificationsType: string = "";
+  notificationContent: string = "";
+  active: string = ""; 
+  notificationDate: Date; 
+}

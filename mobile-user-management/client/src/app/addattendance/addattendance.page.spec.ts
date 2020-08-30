a744147aff9cf7d97abf720bcc3e80b55c4d0273
santhosh <santhurajakaidale@gmail.com>
2020-08-30 21:08:01 +0530

    Updated code for attendance view

---
+++ mobile-user-management/client/src/app/addattendance/addattendance.page.spec.ts
@@ -0,0 +1,28 @@
+
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddAttendancePage } from './addattendance.page';
+
+describe('AttendancePage', () => {
+  let component: AddAttendancePage;
+  let fixture: ComponentFixture<AddAttendancePage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddAttendancePage ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddAttendancePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ mobile-user-management/client/src/app/addattendance/addattendance.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addattendance',
+  templateUrl: './addattendance.page.html',
+  styleUrls: ['./addattendance.page.scss'],
+})
+export class AddAttendancePage implements OnInit {
+  months = [
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
